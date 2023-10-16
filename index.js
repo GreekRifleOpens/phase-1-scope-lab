c916b195d586884621b3f0ee12d2e3409ea35b82
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-10-16 21:35:35 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const LeastFavoriteCustomer = 'some inital value'
+
+upperCaseCustomerName = () => {
+    return customerName = customerName.toUpperCase();
+};
+
+setBestCustomer = () => {
+    return bestCustomer = `not bob`
+}
+
+overwriteBestCustomer = () => {
+    return bestCustomer = `maybe bob`
+}
+
+changeLeastFavoriteCustomer = () => {
+    return LeastFavoriteCustomer = 'boy'
+}
